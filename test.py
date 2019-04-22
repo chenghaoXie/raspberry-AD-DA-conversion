67e887415ec5962ae37dd1b660c9b896d693232b
chenghaoXie <32785793+chenghaoXie@users.noreply.github.com>
2019-04-22 09:45:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ getV = CDLL('./getVoltage.so')
  
 #Find sum of integers
 #print('go to load\n')
+getV.getVoltage.restype = POINTER(c_long)
 res_int32_t = getV.getVoltage()
-res_int32_t.restype = POINTER(c_long)
 print (res_int32_t)
